3cf89018b88ccd75839fabf3c769bff473e7fc4e
Alassane SANOGO <alassanesanogojumeau@gmail.com>
2024-10-23 12:40:21 +0000

    hastag #

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -133,7 +133,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash:true})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
